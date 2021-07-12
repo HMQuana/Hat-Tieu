03b1dc747fc4a5288a8ff9f93a24c82c397b9b88
HMQuana <86846797+HMQuana@users.noreply.github.com>
2021-07-12 18:24:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Núi à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Hê lổ Hê lô!',
+    introDesc: `Ngày hôm nay ta cùng hợp hoan nơi đây
+    Mọi người bên nhau ta hát mừng sinh nhật
+    1 2 3
+    Ta cùng thổi tắt nến ',
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là chồng của em <33',
-    btnNo: 'Không, ông im mẹ đi -.-!',
-    question:'Trên thế giới hơn 7 tỉ người mà sao ta lại gặp nhau nhỉ <3',
+    title: 'Happy Birthday 😙',
+    desc: 'Happy Birthday to you 😗😗 ',
+    btnYes: 'Vẫn cứ là xịn chồng của em <33',
+    btnNo: 'Mỗi thế thôi à -.-!',
+    question:'Trên thế giới hơn 7 tỉ người mà sao có mỗi em là thiên thần thôi nhỉ <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'anh biết mà 🥰. lớp diêu 😘😘',
-    messDesc: 'hnay sinh nhật cho làm công chúa 1 hôm nhé... sinh nhật vui vẻ, ko nói nhiều, phải hạnh phúc... mai anh qua đón đi chơi <3',
+    reply: 'Tại vì em là Hạt Tiêu Cute <33333333',
+    mess: 'anh biết mà 🥰😘😘',
+    messDesc: 'hnay sinh nhật cho làm công chúa 1 hôm nhé... sinh nhật vui vẻ, ko nói nhiều, phải hạnh phúc... Hẹn 1 ngày không xa đi bay nhảy Thủ Đô <3',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
